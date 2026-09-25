a0c4905b52d06e2f4c18ea3e68070699a12c38b3
NicoMadja <nicoarim@gmail.com>
2026-09-25 20:16:30 -0300

    comentario

--- Initial_Fantasy/src/Modelo/Mapa/Nodos/Tienda.java
+++ Initial_Fantasy/src/Modelo/Mapa/Nodos/Tienda.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Tienda {
     private List<Consumible> catalogo;
 
-
+    // oor ahi es mejor consultarle directamente al consumible su precio
     public int consultarPrecio(Consumible item) {
         return item.getPrecio();
     }
